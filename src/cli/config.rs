2f781af05331443d127e00418d334e8ee2d1e353
real-davidwilson <davidwilsonbr2019@gmail.com>
2023-06-06 15:24:12 -0300

    camel case pattern

--- src/cli/config.rs
+++ src/cli/config.rs
@@ -1,7 +1,9 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Config {
     pub cmd: String,
     pub path: String,
+    pub delay: usize,
 }
